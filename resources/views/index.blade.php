09fafe864255fe258651d5e9a65eeb239e964ad4
tariq_sks <muhammad.tariq@source-kode.com>
2022-11-12 23:11:26 +0500

    some changes are done in index file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,25 +3,19 @@
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <meta charset="utf-8">
     <meta name="csrf-token" content="content">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Laravel Dependent AJAX Dropdown Tutorial</title>
+    <title>Ajax JQuery Work</title>
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <title>Ajax Validation</title>
-
-
+    <title>Ajax Jqeury Working</title>
     <style>
         body{
             margin: auto;
@@ -70,17 +64,12 @@
                     </select>
                   </div>
               </div>
-
             <button type="submit" id="submit" class="btn btn-primary">Form Submit</button>
           </form>
-
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script>
         $(document).ready(function () {
             $('#country').on('change', function () {
-                // var idCountry = $(this).val();
-                // console.log(idCountry);
-
                 $.ajax({
                     url: "{{ route('stateData') }}",
                     type: "POST",
@@ -92,7 +81,6 @@
                     success: function (result) {
                         $('#state').html('<option value="">Select State</option>');
                         $.each(result['states'], function (key, value) {
-                        // console.log(value['state_name']);
                             $("#state").append('<option value="' + value
                             .id + '">' + value['state_name'] + '</option>');
                         });
@@ -120,12 +108,5 @@
             });
         });
     </script>
-
-
     </body>
 </html>
-
-
-
-
-
